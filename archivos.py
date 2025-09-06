1234d064594ed383a519071c7da0a0d54ccfeed5
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2025-09-06 19:58:06 -0300

    extracting data from files

--- archivos.py
+++ archivos.py
@@ -1,8 +1,18 @@
-import numpy as np
-import scipy as sp
 import h5py
+import matplotlib.pyplot as plt
+
+# esta pagina te permite ver el contenido de los archivos: myhdf5.hdfgroup.org/
 
 h1 = h5py.File("H-H1_GWOSC_4KHZ_R1-1126257415-4096.hdf5", "r")
 l1 = h5py.File("L-L1_GWOSC_4KHZ_R1-1126257415-4096.hdf5", "r")
 # los archivos estan organizados como un Python dictionary
-# los keys son: ['meta', 'quality', 'strain']
+# los datos estan guardados en "strain" --> "Strain"
+
+strainH1 = h1["strain"]["Strain"][()]
+strainL1 = l1["strain"]["Strain"][()]
+sampleRate = 4096  # [Hz]
+
+# para ver la forma de los datos
+# plt.plot(strainH1)
+# plt.plot(strainL1)
+# plt.show()
